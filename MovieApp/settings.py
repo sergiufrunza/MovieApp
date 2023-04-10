775645839b749354154b314c96e0ca5f6354c382
sergiufrunza <sergiu.frunza120@gmail.com>
2023-04-11 01:57:54 +0300

    dev

--- MovieApp/settings.py
+++ MovieApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -71,8 +72,12 @@ WSGI_APPLICATION = 'MovieApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3' ,
-        'NAME': BASE_DIR / 'db.sqlite3' ,
+        'HOST': config("DATABASE_HOST"),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': config("DATABASE_USER"),
+        'PASSWORD': config("DATABASE_PASS"),
+        'NAME': config("DATABASE_NAME"),
+        'PORT': config("DATABASE_PORT")
     }
 }
 
