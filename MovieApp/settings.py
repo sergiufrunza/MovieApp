5c9f0165c213703cadf285cfe110112a2bf0307c
sergiufrunza <sergiu.frunza120@gmail.com>
2023-04-11 21:43:02 +0300

    dev

--- MovieApp/settings.py
+++ MovieApp/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=1))
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split()
 # Application definition
 
 INSTALLED_APPS = [
