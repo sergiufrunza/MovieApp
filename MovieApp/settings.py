9dd54d96d07bd76a072c14e38e0be1615429f66a
sergiufrunza <sergiu.frunza120@gmail.com>
2023-04-12 01:20:33 +0300

    dev1

--- MovieApp/settings.py
+++ MovieApp/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 DEBUG = config("DEBUG", default=1)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(" ")
+SECRET_KEY = config('SECRET_KEY')
 # Application definition
 
 INSTALLED_APPS = [
