52937b105b104ecb52022902bf200b06bc6f3ab4
sergiufrunza <sergiu.frunza120@gmail.com>
2023-04-12 23:05:47 +0300

    testdev1

--- MovieApp/settings.py
+++ MovieApp/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions' ,
     'django.contrib.messages' ,
     'django.contrib.staticfiles' ,
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -79,23 +80,26 @@ DATABASES = {
     }
 }
 
-if config("DEBUG") == 20:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    LINODE_BUCKET=config("BUCKET_NAME")
-    LINODE_BUCKET_REGION=config("BUCKET_REGION")
-    LINODE_BUCKET_ACCESS_KEY=config("LINODE_BUCKET_ACCESS_KEY")
-    LINODE_BUCKET_SECRET_KEY=config("LINODE_BUCKET_SECRET_KEY")
-
-
-    AWS_S3_ENDPOINT_URL=f'https://{LINODE_BUCKET_REGION}.linodeobjects.com'
-    AWS_ACCESS_KEY_ID=LINODE_BUCKET_ACCESS_KEY
-    AWS_SECRET_ACCESS_KEY=LINODE_BUCKET_SECRET_KEY
-    AWS_S3_REGION_NAME=LINODE_BUCKET_REGION
-    AWS_S3_USE_SSL=True
-    AWS_STORAGE_BUCKET_NAME=LINODE_BUCKET
+if config("DEBUG") == 0:
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = None
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    STATIC_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATICFILES_STORAGE = 'MovieApp.storage_backends.StaticStorage'
+    # s3 public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'MovieApp.storage_backends.PublicMediaStorage'
 else:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     MEDIA_URL = '/media/'
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Password validation
@@ -130,10 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR ,"static")
-MEDIA_ROOT = os.path.join(BASE_DIR ,"media")
-MEDIA_URL = "/media/"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
