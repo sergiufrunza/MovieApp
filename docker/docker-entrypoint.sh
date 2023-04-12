0c73b89beee4d09c5eb6b49a7e2c8fc992f1dd2e
sergiufrunza <sergiu.frunza120@gmail.com>
2023-04-13 01:51:49 +0300

    testdev1

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,6 +2,6 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py createsuperuser_if_none_exists --user=${SUPER_USER_NAME} --password=${SUPER_USER_PASS}
+python manage.py createsuperuser_if_none_exists --user=$SUPER_USER_NAME --password=$SUPER_USER_PASS
 
 /usr/local/bin/supervisord
