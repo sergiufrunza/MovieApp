11385a18f907f01d2c694b090d3a0e31cfb19baa
sergiufrunza <sergiu.frunza120@gmail.com>
2023-04-13 19:55:22 +0300

    dev2

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,6 +2,6 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py createsuperuser_if_none_exists
+#python manage.py createsuperuser_if_none_exists
 
 /usr/local/bin/supervisord
