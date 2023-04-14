dd0cca1f7ae168fd4abc2f25472c7edd2c445752
sergiufrunza <sergiu.frunza120@gmail.com>
2023-04-15 02:57:09 +0300

    test5

--- web/MovieApp/settings.py
+++ web/MovieApp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = config('DEBUG') == 'True'
+DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(" ")
 SECRET_KEY = config('SECRET_KEY')
 # Application definition
