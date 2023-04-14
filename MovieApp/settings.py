443a0d7a6e46d15ebf85f980cdbeff227d0005f5
sergiufrunza <sergiu.frunza120@gmail.com>
2023-04-14 13:00:40 +0300

    test

--- MovieApp/settings.py
+++ MovieApp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = int(config('DEBUG'))
+DEBUG = config('DEBUG', cast=int)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(" ")
 SECRET_KEY = config('SECRET_KEY')
 # Application definition
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions' ,
     'django.contrib.messages' ,
     'django.contrib.staticfiles' ,
-    'storages',
+    'storages' ,
 ]
 
 MIDDLEWARE = [
@@ -73,38 +73,45 @@ WSGI_APPLICATION = 'MovieApp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'HOST': config('DATABASE_HOST'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASS'),
-        'NAME': config('DATABASE_NAME'),
-        'PORT': config('DATABASE_PORT')
+if config('DEBUG',cast=int):
+    DATABASES = {
+        'default': {
+            'HOST': config('DATABASE_HOST'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'USER': config('DATABASE_USER'),
+            'PASSWORD': config('DATABASE_PASS'),
+            'NAME': config('DATABASE_NAME'),
+            'PORT': config('DATABASE_PORT')
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-# if int(config('DEBUG')):
-#     AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID_BUCKET')
-#     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY_BUCKET')
-#     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-#     AWS_DEFAULT_ACL = None
-#     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-#     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-#     # s3 static settings
-#     STATIC_LOCATION = 'static'
-#     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-#     STATICFILES_STORAGE = 'MovieApp.storage_backends.StaticStorage'
-#     # s3 public media settings
-#     PUBLIC_MEDIA_LOCATION = 'media'
-#     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-#     DEFAULT_FILE_STORAGE = 'MovieApp.storage_backends.PublicMediaStorage'
-# else:
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+if config('DEBUG',cast=int):
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID_BUCKET')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY_BUCKET')
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = None
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    STATIC_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATICFILES_STORAGE = 'MovieApp.storage_backends.StaticStorage'
+    # s3 public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'MovieApp.storage_backends.PublicMediaStorage'
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_URL = '/media/'
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Password validation
